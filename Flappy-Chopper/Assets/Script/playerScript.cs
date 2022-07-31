1859495aadb222b5504fc2229a6b3f94a1fad9f6
Mattia Raffaele <raffaelemattia22@gmail.com>
2022-07-31 15:40:56 +0200

    +birdy +more inputs +

--- Flappy-Chopper/Assets/Script/playerScript.cs
+++ Flappy-Chopper/Assets/Script/playerScript.cs
@@ -6,22 +6,35 @@ public class playerScript : MonoBehaviour
 
 
     public float rbVerticalVelocity;
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite frame1;
+    public Sprite frame2;
+    public Sprite frame3;
+
+    private bool canJump;
  
 
 
     void Start()
     {
+        canJump = true;
         rb = GetComponent<Rigidbody2D>();
     }
 
 
-
-
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
+        Debug.Log("Jumpstate" + canJump);
+
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0 && canJump)
         {
+            canJump = false;
+
+            
             rb.velocity = new Vector2(0f, rbVerticalVelocity);
+            
+            canJump = true;
         }
     }
 }
