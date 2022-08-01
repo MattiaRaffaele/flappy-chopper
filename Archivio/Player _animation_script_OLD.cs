c7014a07d300b2c39ffb9e536834b4e4332966d7
Mattia Raffaele <raffaelemattia22@gmail.com>
2022-08-01 23:30:33 +0200

    arch

---
+++ Archivio/Player _animation_script_OLD.cs
@@ -0,0 +1,79 @@
+//Mattia Raffaele
+//01 08 2022
+//Lunedì
+//Animazione birdy 3 frame
+//Sostituito con rotazione flappy
+
+
+
+
+using UnityEngine;
+using System.Collections;
+
+
+public class playerScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+
+    public float rbVerticalVelocity;
+
+    [SerializeField] SpriteRenderer spriteRenderer;
+ 
+
+    public Sprite[] sprite;
+
+    private bool canJump;
+ 
+
+
+    void Start()
+    {
+        canJump = true;
+
+        //dichiarazioni
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+
+        if (canJump && Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0)
+        {
+            //sprite [0] = idle version
+            //sprite [1] = jumping version
+            //sprite [2] = afterjump version
+
+            Debug.Log("jumpPhase started");
+
+
+            
+            //rb jump
+            rb.velocity = new Vector2(0f, rbVerticalVelocity);
+
+            spriteRenderer.sprite = sprite[1];
+
+            //Debug
+            Debug.Log("Sprite changes IENumerator running");
+
+            //IE starts
+            animationDeelay();
+
+            
+            spriteRenderer.sprite = sprite[2];
+
+            afterjump();
+        }
+    }
+
+    IEnumerator animationDeelay(){
+        yield return new WaitForSeconds(100);
+    }
+
+    private void afterjump(){
+        spriteRenderer.sprite = sprite[0];
+        canJump = true;
+        Debug.Log("jumpPhase ended");
+    }
+}
--- Flappy-Chopper/Assets/Script/playerScript.cs
+++ Flappy-Chopper/Assets/Script/playerScript.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
 
+
 public class playerScript : MonoBehaviour
 {
     Rigidbody2D rb;
 
-
     public float rbVerticalVelocity;
-
-    public SpriteRenderer spriteRenderer;
-    public Sprite frame1;
-    public Sprite frame2;
-    public Sprite frame3;
 
     private bool canJump;
  
@@ -19,21 +14,20 @@ public class playerScript : MonoBehaviour
     void Start()
     {
         canJump = true;
+
+        //dichiarazioni
         rb = GetComponent<Rigidbody2D>();
     }
 
 
     private void Update()
     {
-        Debug.Log("Jumpstate" + canJump);
 
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0 && canJump)
-        {
-            canJump = false;
-
-            
+        if (canJump && Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0)
+        {           
+            //rb jump
             rb.velocity = new Vector2(0f, rbVerticalVelocity);
-            
+
             canJump = true;
         }
     }
